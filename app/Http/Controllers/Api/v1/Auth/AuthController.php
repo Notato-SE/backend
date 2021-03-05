7d33b3331fc26aa5ee2fc82700a50094ba01ac96
Vichea <mr.vichea.007@gmail.com>
2021-03-05 23:36:08 +0700

    Fix Bug

--- app/Http/Controllers/Api/v1/Auth/AuthController.php
+++ app/Http/Controllers/Api/v1/Auth/AuthController.php
@@ -26,7 +26,6 @@ class AuthController extends ApiController
             "password" => ["required", "string", "max:255"],
         ]);
 
-        dd(1);
         if (!Auth::attempt($credential)) {
             throw new AuthenticationException('Username or password is wrong.');
         }
