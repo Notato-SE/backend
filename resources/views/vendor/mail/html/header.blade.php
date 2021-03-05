a1fb5bf0b4e1ce03ff952197ba0de884e104a802
Vichea <mr.vichea.007@gmail.com>
2021-03-06 00:08:11 +0700

    Fix Bug

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
     <td class="header">
         <a href="{{ config('app.front_end_url') }}" style="display: inline-block;">
-            <img src="{{ asset('storage/images/logo.png') }}" class="logo" alt="Laravel Logo">
+            <img src="{{ asset('images/logo.png') }}" class="logo" alt="Laravel Logo">
         </a>
     </td>
 </tr>
